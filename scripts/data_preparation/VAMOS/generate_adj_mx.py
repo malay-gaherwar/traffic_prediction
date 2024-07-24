49dbed02965f23cb5ec22459c3ba67f330b8be8c
Malay_Gaherwar <malaygsingh@gmail.com>
2024-07-24 16:01:58 +0200

    Adjacency matrix generation python file edited. Working now. Stiil need to check how indexes in this and training data would match.

--- scripts/data_preparation/VAMOS/generate_adj_mx.py
+++ scripts/data_preparation/VAMOS/generate_adj_mx.py
@@ -6,22 +6,27 @@ import numpy as np
 import pandas as pd
 from geopy.distance import geodesic
 
-def get_adjacency_matrix_vamos(distance_df_filename: str, num_of_vertices: int) -> tuple:
-    """Generate adjacency matrix for VAMOS data.
+def extract_lat_long(geom):
+    if pd.isna(geom):
+        return None, None
+    geom = geom.strip("POINT ()")
+    lat, long = map(float, geom.split())
+    return lat, long
 
-    Args:
-        distance_df_filename (str): path of the csv file containing sensor information
-        num_of_vertices (int): number of vertices
+def get_adjacency_matrix_vamos(distance_df_filename: str):
+   
+    data = pd.read_csv(distance_df_filename)
 
-    Returns:
-        tuple: two adjacency matrices
-            np.array: fully connected adjacency matrix (all entries are 1)
-            np.array: distance-based adjacency matrix (geodesic distances)
-    """
+    # Ignore sensors where "geom" is empty
+    data = data.dropna(subset=["geom"])
+
+    data[['longitude', 'latitude']] = data['geom'].apply(lambda x: pd.Series(extract_lat_long(x)))
+
+    num_of_vertices = len(data)
     adjacency_matrix_connectivity = np.ones((num_of_vertices, num_of_vertices), dtype=np.float32)
     adjacency_matrix_distance = np.zeros((num_of_vertices, num_of_vertices), dtype=np.float32)
 
-    data = pd.read_csv(distance_df_filename)
+    # Calculate distance-based adjacency matrix
     for i in range(num_of_vertices):
         for j in range(i + 1, num_of_vertices):
             distance = geodesic((data.iloc[i]['latitude'], data.iloc[i]['longitude']),
@@ -32,19 +37,9 @@ def get_adjacency_matrix_vamos(distance_df_filename: str, num_of_vertices: int) 
     return adjacency_matrix_connectivity, adjacency_matrix_distance
 
 def generate_adj_vamos():
-    distance_df_filename = "datasets/raw_data/VAMOS/sample_vamos_data_2022_23.csv"
-    num_of_vertices = len(pd.read_csv(distance_df_filename))
+    distance_df_filename = "datasets/raw_data/VAMOS/pcs_meta.csv"
 
-    adj_mx, distance_mx = get_adjacency_matrix_vamos(distance_df_filename, num_of_vertices)
-    
-    # Note: For VAMOS, adding a self-loop might not be necessary, if required uncomment below.
-    add_self_loop = False
-    if add_self_loop:
-        print("adding self loop to adjacency matrices.")
-        adj_mx = adj_mx + np.identity(adj_mx.shape[0])
-        distance_mx = distance_mx + np.identity(distance_mx.shape[0])
-    else:
-        print("kindly note that there is no self loop in adjacency matrices.")
+    adj_mx, distance_mx = get_adjacency_matrix_vamos(distance_df_filename)
         
     with open("datasets/raw_data/VAMOS/adj_VAMOS.pkl", "wb") as f:
         pickle.dump(adj_mx, f)
