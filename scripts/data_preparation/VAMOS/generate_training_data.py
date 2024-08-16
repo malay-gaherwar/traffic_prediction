b86137325a4035d21c5a66eba68008fc78aad66c
Jyotirmaya Ijaradar <jyotirmaya.ijaradar@tu-dresden.de>
2024-08-16 13:03:50 +0200

    checked and modified data processing file

--- scripts/data_preparation/VAMOS/generate_training_data.py
+++ scripts/data_preparation/VAMOS/generate_training_data.py
@@ -15,7 +15,7 @@ from basicts.data.transform import standard_transform
 
 """
 def generate_adj_matrix(data, threshold_distance=500):
-    
+
     num_sensors = len(data)
     adjacency_matrix = np.zeros((num_sensors, num_sensors))
     for i in range(num_sensors):
@@ -28,6 +28,7 @@ def generate_adj_matrix(data, threshold_distance=500):
     return adjacency_matrix
 
 """
+
 
 def generate_data(args: argparse.Namespace):
     """Preprocess and generate train/valid/test datasets for Vamos data."""
@@ -69,9 +70,13 @@ def generate_data(args: argparse.Namespace):
 
     print("raw time series shape: ", data.shape)
 
+    # Reshape the data to add a third dimension for features
+    data = data.values[:, :, np.newaxis]  # Add a third dimension for features
+
+    # Now unpack the shape
+    l, n, f = data.shape
 
     # split data
-    l, n , f = data.shape
     num_samples = l - (history_seq_len + future_seq_len) + 1
     train_num = round(num_samples * train_ratio)
     valid_num = round(num_samples * valid_ratio)
@@ -82,7 +87,7 @@ def generate_data(args: argparse.Namespace):
 
     index_list = []
     for t in range(history_seq_len, num_samples + history_seq_len):
-        index = (t-history_seq_len, t, t+future_seq_len)
+        index = (t - history_seq_len, t, t + future_seq_len)
         index_list.append(index)
 
     train_index = index_list[:train_num]
@@ -92,33 +97,35 @@ def generate_data(args: argparse.Namespace):
 
     # normalize data
     scaler = standard_transform
-    data_norm = scaler(data, output_dir, train_index, history_seq_len, future_seq_len, norm_each_channel=norm_each_channel)
+    data_norm = scaler(data, output_dir, train_index, history_seq_len, future_seq_len,
+                       norm_each_channel=norm_each_channel)
 
     # add temporal feature
     feature_list = [data_norm]
     if add_time_of_day:
-        tod = (
-            df.index.values - df.index.values.astype("datetime64[D]")) / np.timedelta64(1, "D")
-        tod_tiled = np.tile(tod, [1, n, 1]).transpose((2, 1, 0))
-        feature_list.append(tod_tiled)
+        tod = (df_resampled.index.values - df_resampled.index.values.astype("datetime64[D]")) / np.timedelta64(1,
+                                                                                                                      "D")
+        tod_tiled = np.expand_dims(tod, axis=-1)
+        tod_tiled = np.repeat(tod_tiled, n, axis=1)
+        feature_list.append(np.expand_dims(tod_tiled, axis=-1))  # Ensure 3D shape
 
     if add_day_of_week:
-        # numerical day_of_week
-        dow = df.index.dayofweek / 7
-        dow_tiled = np.tile(dow, [1, n, 1]).transpose((2, 1, 0))
-        feature_list.append(dow_tiled)
-    
+        dow = df_resampled.index.dayofweek / 7
+        dow_tiled = np.expand_dims(dow, axis=-1)
+        dow_tiled = np.repeat(dow_tiled, n, axis=1)
+        feature_list.append(np.expand_dims(dow_tiled, axis=-1))  # Ensure 3D shape
+
     if add_day_of_month:
-        # numerical day_of_month
-        dom = (df.index.day - 1 ) / 31 # df.index.day starts from 1. We need to minus 1 to make it start from 0.
-        dom_tiled = np.tile(dom, [1, n, 1]).transpose((2, 1, 0))
-        feature_list.append(dom_tiled)
+        dom = (df_resampled.index.day - 1) / 31  # df.index.day starts from 1. We need to minus 1 to make it start from 0.
+        dom_tiled = np.expand_dims(dom, axis=-1)
+        dom_tiled = np.repeat(dom_tiled, n, axis=1)
+        feature_list.append(np.expand_dims(dom_tiled, axis=-1))  # Ensure 3D shape
 
     if add_day_of_year:
-        # numerical day_of_year
-        doy = (df.index.dayofyear - 1) / 366 # df.index.month starts from 1. We need to minus 1 to make it start from 0.
-        doy_tiled = np.tile(doy, [1, n, 1]).transpose((2, 1, 0))
-        feature_list.append(doy_tiled)
+        doy = (df_resampled.index.dayofyear - 1) / 366  # df.index.month starts from 1. We need to minus 1 to make it start from 0.
+        doy_tiled = np.expand_dims(doy, axis=-1)
+        doy_tiled = np.repeat(doy_tiled, n, axis=1)
+        feature_list.append(np.expand_dims(doy_tiled, axis=-1))  # Ensure 3D shape
 
     processed_data = np.concatenate(feature_list, axis=-1)
 
@@ -127,13 +134,17 @@ def generate_data(args: argparse.Namespace):
     index["train"] = train_index
     index["valid"] = valid_index
     index["test"] = test_index
-    with open(output_dir + "/index_in_{0}_out_{1}_rescale_{2}.pkl".format(history_seq_len, future_seq_len, if_rescale), "wb") as f:
+    with open(output_dir + "/index_in_{0}_out_{1}_rescale_{2}.pkl".format(history_seq_len, future_seq_len, if_rescale),
+              "wb") as f:
         pickle.dump(index, f)
 
     data = {}
     data["processed_data"] = processed_data
-    with open(output_dir + "/data_in_{0}_out_{1}_rescale_{2}.pkl".format(history_seq_len, future_seq_len, if_rescale), "wb") as f:
+    with open(output_dir + "/data_in_{0}_out_{1}_rescale_{2}.pkl".format(history_seq_len, future_seq_len, if_rescale),
+              "wb") as f:
         pickle.dump(data, f)
+
+
 """
     # generate and save adjacency matrix
     adj_matrix = generate_adj_matrix(df)
@@ -148,17 +159,17 @@ if __name__ == "__main__":
 
     TRAIN_RATIO = 0.6
     VALID_RATIO = 0.2
-    TARGET_CHANNEL = [0, 1, 2, 3, 4, 5]    # target channels
+    TARGET_CHANNEL = [0, 1, 2, 3, 4, 5]  # target channels
     STEPS_PER_DAY = 288
 
     DATASET_NAME = "VAMOS"
-    TOD = True                  # if add time_of_day feature
-    DOW = True                  # if add day_of_week feature
-    DOM = True                  # if add day_of_month feature
-    DOY = True                  # if add day_of_year feature
+    TOD = True  # if add time_of_day feature
+    DOW = True  # if add day_of_week feature
+    DOM = True  # if add day_of_month feature
+    DOY = True  # if add day_of_year feature
 
     OUTPUT_DIR = "datasets/" + DATASET_NAME
-    DATA_FILE_PATH = "datasets/raw_data/VAMOS/sample_vamos_data_2022_23.csv"
+    DATA_FILE_PATH = "/home/ijaradar/Work/dl-trafic-prediction-gsm/datasets/raw_data/VAMOS/vamos_394-97_sample.csv"
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--output_dir", type=str,
